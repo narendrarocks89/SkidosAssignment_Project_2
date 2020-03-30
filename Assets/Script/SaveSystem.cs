1dd1ce102e345cd894ff0ef74c0874c5a9f2f43a
Narendra Chaudhary <narendrarocks89@gmail.com>
2020-03-31 01:15:23 +0530

    Final update v0.1

---
+++ Assets/Script/SaveSystem.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class SaveSystem
+{
+    public static readonly string SAVE_FOLDER = "D:/Datazzz/Unity Projects/Personal/SKIDOS Assignment/SkidosAssignment_Project_1/Assets/Json Files";
+    
+    public static void Init()
+    {
+        if (!Directory.Exists(SAVE_FOLDER))
+        {
+            Directory.CreateDirectory(SAVE_FOLDER);
+        }
+    }
+
+    public static string Load()
+    {
+        if (File.Exists(SAVE_FOLDER + "/save.json"))
+        {
+            string saveString = File.ReadAllText(SAVE_FOLDER + "/save.json");
+            return saveString;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private static string GetAndroidExternalFilesDir()
+{
+     using (AndroidJavaClass unityPlayer = 
+            new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+     {
+          using (AndroidJavaObject context = 
+                 unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+          {
+               // Get all available external file directories (emulated and sdCards)
+               AndroidJavaObject[] externalFilesDirectories = 
+                                   context.Call<AndroidJavaObject[]>
+                                   ("getExternalFilesDirs", (object)null);
+               AndroidJavaObject emulated = null;
+               AndroidJavaObject sdCard = null;
+               for (int i = 0; i < externalFilesDirectories.Length; i++)
+               {
+                    AndroidJavaObject directory = externalFilesDirectories[i];
+                    using (AndroidJavaClass environment = 
+                           new AndroidJavaClass("android.os.Environment"))
+                    {
+                        // Check which one is the emulated and which the sdCard.
+                        bool isRemovable = environment.CallStatic<bool>
+                                          ("isExternalStorageRemovable", directory);
+                        bool isEmulated = environment.CallStatic<bool>
+                                          ("isExternalStorageEmulated", directory);
+                        if (isEmulated)
+                            emulated = directory;
+                        else if (isRemovable && isEmulated == false)
+                            sdCard = directory;
+                    }
+               }
+               // Return the sdCard if available
+               if (sdCard != null)
+                    return sdCard.Call<string>("getAbsolutePath");
+               else
+                    return emulated.Call<string>("getAbsolutePath");
+            }
+      }
+}
+}
---
+++ Assets/Script/SubmitData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+using System.IO;
+public class SubmitData : MonoBehaviour
+{
+    public TextMeshProUGUI keyText;
+    public TextMeshProUGUI ValueText;
+
+    public class PlayerData
+    {
+        public string Key;
+        public string Value;
+    }
+
+    public void Fetch()
+    {
+        string saveString = SaveSystem.Load();
+
+        if (saveString != null)
+        {
+            PlayerData playerData = JsonUtility.FromJson<PlayerData>(saveString);
+
+            Debug.Log("Key : " + playerData.Key);
+            Debug.Log("Value : " + playerData.Value);
+
+            keyText.text = "KEY: " + playerData.Key;
+            ValueText.text = "VALUE: " + playerData.Value;
+        }
+        else
+        {
+            Debug.Log("File is missing.");
+        }
+    }
+}
